cac66141e2268fbed05261adf3ccba2ec2a39f60
Josué Llopez <llopezjosue31@gmail.com>
2020-06-08 11:34:17 +0200

    code ok

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,11 +1,15 @@
 const assert = require('assert');
 
-// WRITE THE ACTUAL FUNCTION HERE
+function capitalizeFirstLetter(str) {
+        return str.replace(/\w\S*/g, function(txt){
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+}
 
 // Check that capitalizeFirst is a function
 assert.strictEqual(typeof capitalizeFirstLetter, 'function');
 // Check that capitalizeFirst transforms javaScript correctly
-assert.strictEqual(capitalizeFirstLetter('javaScript is magic'), 'JavaScript Is Magic');
+assert.strictEqual(capitalizeFirstLetter('javascript is magic'), 'Javascript Is Magic');
 // Check that it works for a 1-character string
 assert.strictEqual(capitalizeFirstLetter('z'), 'Z');
 // Check that it works for an empty string
